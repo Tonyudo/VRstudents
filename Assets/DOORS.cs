0784cf70ab54fd129e0b00c5e59ff6589edb6d01
unknown <shellysa123@gmail.com>
2018-04-15 11:30:01 +0300

    First commit

---
+++ Assets/DOORS.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOORS : MonoBehaviour {
+
+    Animator animator;
+    bool DoorOpen;
+
+    private void Start()
+    {
+        DoorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+      if(col.gameObject.tag == "Player")
+        {
+            DoorOpen = true;
+            Doors("Open");
+        }  
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (DoorOpen)
+        {
+            DoorOpen = false;
+            Doors("Close");
+        }
+    }
+    void Doors(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
---
+++ Assets/scripts/DataController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour
+{
+    public RoundData[] allRoundData;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.LoadScene("Game");
+    }
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
